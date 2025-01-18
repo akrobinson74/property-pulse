1c5470cf1fd9d33039e49577769e8f0cabc45232
Adam Robinson <akrobinson74@gmail.com>
2025-01-17 21:26:30 -0600

    trying to fix vercel build issue

--- utils/requests.js
+++ utils/requests.js
@@ -7,8 +7,7 @@ async function fetchProperties({ showFeatured = false } = {}) {
     }
 
     const res = await fetch(
-      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`,
-      { cache: "no-store" }
+      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`
     );
     if (!res.ok) {
       throw new Error("failed to fetch data");
